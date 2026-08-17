d55afbacf4d169d71346a50e2648bd8def0c9e8b
Muhammad Zohaib Arif <mzohaibarif0326@gmail.com>
2026-08-17 15:14:49 +0500

    Make Flask backend Railway-compatible

--- backend.py
+++ backend.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 from chatbot import (
     chat_with_memory,
@@ -257,12 +258,20 @@ if __name__ == "__main__":
     print("=" * 70)
 
 
+    port = int(
+        os.environ.get(
+            "PORT",
+            5000
+        )
+    )
+
+
     app.run(
 
-        host="127.0.0.1",
+        host="0.0.0.0",
 
-        port=5000,
+        port=port,
 
-        debug=True
+        debug=False
 
     )
